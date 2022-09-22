656e3a5ebe35996f9bf5e3a5b1705ec446244401
Scusemua <benrcarver@gmail.com>
2022-09-22 12:42:34 -0400

    Updated script

--- monitor_nns.py
+++ monitor_nns.py
@@ -20,17 +20,25 @@ logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--interval", type = float, default = 0.5, help = "Interval to check for active NNs (in seconds).")
+parser.add_argument("-d", "--duration", type = float, default = 60, help "Duration (in seconds) to monitor for NNs.")
 args = parser.parse_args()
 
 interval = args.interval
+duration_sec = args.duration
+duration_ms = duration_sec * 1000
+
+# Requires Python 3.7
+def current_milli_time():
+    return time.time_ns() // 1_000_000
 
 start_time = datetime.now()
+start = current_milli_time()
 
 if interval <= 0 or interval > 2:
     logger.error("Interval must be within the interval (0, 2).")
 
 res = []
-for i in range(0, 5):
+while (current_milli_time() - start < duration):
   result = subprocess.run(["kubectl", "get", "pods"], stdout=subprocess.PIPE)
   lines = result.stdout.decode().split("\n")
   current_num_nns = 0
@@ -44,7 +52,7 @@ for i in range(0, 5):
   now = datetime.now()
   logger.info("%s: %d NNs" % (now.strftime("%d/%m/%Y %H:%M:%S"), current_num_nns))
   
-  res.append((time.time(), current_num_nns))
+  res.append((current_milli_time(), current_num_nns))
   
   time.sleep(interval)
 
