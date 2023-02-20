f13455c0d4bc8f5fe442565198fce7e0b98cfa03
Scusemua <benrcarver@gmail.com>
2023-02-20 16:13:25 -0500

    Updated script

--- throughput_yaml.py
+++ throughput_yaml.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import glob
 import os
-import pickle 
-import yaml 
+import pickle
+import yaml
 from ast import literal_eval as make_tuple
 import re
 
@@ -39,6 +39,7 @@ parser.add_argument("-u", "--units", default = "ns", type = str, help = "Units o
 parser.add_argument("-c", "--columns", default = ["timestamp", "latency"], nargs='+') # ["timestamp", "latency", "worker_id", "path"]
 parser.add_argument("-o", "--output-path", dest = "output_path", default = None, type = str, help = "Output path to write graph to. If not specified, then no output will be saved.")
 parser.add_argument("--show", action = 'store_true', help = "Show the plot rather than just write it to a file")
+parser.add_argument("--save-dfs", dest = "save_dfs", action = 'store_true', help = "Save the dataframes to a file.")
 parser.add_argument("--legend", action = 'store_true', help = "Show the legend on each plot.")
 parser.add_argument("--cost", action = 'store_true', help = "Show the legend on each plot.")
 parser.add_argument("--no-y-axis-labels", dest = "no_y_axis_labels", action = 'store_true', help = "Do not plot y-axis labels.")
@@ -60,6 +61,7 @@ cpu_per_nn = args.cpu
 mem_per_nn = args.memory
 plot_cost = args.cost
 no_y_axis_labels = args.no_y_axis_labels
+save_dfs = args.save_dfs
 
 c2_standard_16_cost_per_second = 0.9406 / (60 * 60)
 cpu_cost_per_ms = 0.03827 / (60 * 60 * 1000) # Divide cost-per-hour by 60 min/hr * 60 sec/min * 1000 ms/sec.
@@ -97,7 +99,7 @@ if not no_y_axis_labels:
     axs.set_ylabel("Throughput (ops/sec)", color = 'black')
 axs.yaxis.set_major_formatter(ticker.EngFormatter(sep=""))
 axs.yaxis.set_major_locator(ticker.MultipleLocator(25_000))
-secondary_axis = None 
+secondary_axis = None
 
 def compute_cost_of_operation(row):
     end_to_end_latency_ms = row["latency"]
@@ -107,7 +109,7 @@ def plot(input:dict):
     global color_idx
     global marker_idx
     global secondary_axis
-    
+
     input_path = input["path"]
     label = input.get("label", "No-Label-Specified")
 
@@ -118,109 +120,109 @@ def plot(input:dict):
     linewidth = input.get("linewidth", 4) or 4
     markevery = input.get("markevery", 0.1)
     secondary_label = input.get("secondarylabel", None)
-    secondary_path = input.get("secondarypath", None) 
+    secondary_path = input.get("secondarypath", None)
     buckets_path = input.get("buckets-path", None)
-        
+
     if secondary_path is not None and secondary_axis is None:
         print("Creating secondary axis!")
         secondary_axis = axs.twinx()
-    
+
     if "linecolor" in input:
         linecolor = input["linecolor"]
-        
-        # If the color is specified as 6-character hex, then prepend it with a '#' 
+
+        # If the color is specified as 6-character hex, then prepend it with a '#'
         if len(linecolor) == 6 and re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', "#" + linecolor):
             linecolor = "#" + linecolor
     else:
         linecolor = 'black'
-    
-    # The user may have specified something like (10, (10, 10)), which is a format matplotlib interprets to add offset and whatnot to the dashes. 
+
+    # The user may have specified something like (10, (10, 10)), which is a format matplotlib interprets to add offset and whatnot to the dashes.
     if type(linestyle) is str:
         try:
             linestyle = make_tuple(linestyle)
             print("Setting linestyle to tuple: %s (type is %s)" % (str(linestyle), type(linestyle)))
         except:
-            pass 
-    
+            pass
+
     print("Marker: %s\nMarker Size: %s\nLine style: %s\nLine color: %s\nLine width: %s" % (str(marker), str(markersize), linestyle, linecolor, str(linewidth)))
 
     start_time = time.time()
-    
-    # If we pass a single .txt file, then just create DataFrame from the .txt file.
-    # Otherwise, merge all .txt files in the specified directory.
-    if input_path.endswith(".txt") or input_path.endswith(".csv"):
-        print("Reading existing DF from file at '%s'" % input_path)
-        df = pd.read_csv(input_path, index_col=None, header=0)
-        print("Existing DF has the following columns: %s" % str(df.columns))
-        if len(df.columns) == 2:
-            df.columns = COLUMNS
-            print("Set DF's columns to %s" % str(COLUMNS))
-        
-        print("Loaded existing DF in %f seconds" % (time.time() - start_time))
-    else:
-        print("input_path: " + input_path)
-        print("joined: " + str(os.path.join(input_path, "*.txt")))
-        all_files = glob.glob(os.path.join(input_path, "*.txt"))
-        li = []
-        print("Merging the following files: %s" % str(all_files))
-        # Merge the .txt files into a single DataFrame.
-        for filename in all_files:
-            print("Reading file: " + filename)
-            tmp_df = pd.read_csv(filename, index_col=None, header=0)
-            tmp_df.columns = COLUMNS
-            li.append(tmp_df)
-        df = pd.concat(li, axis=0, ignore_index=True)
-        df.columns = COLUMNS
-        
-        print("Loaded data and created DF in %f seconds" % (time.time() - start_time))
 
-    st_time = time.time()
-    if 'ts' not in df.columns:
-        # Sort the DataFrame by timestamp.
-        print("Sorting DF and creating `ts` column now...")
-        start_sort = time.time()
-        df = df.sort_values('timestamp')
-        print("Sorted dataframe in %f seconds." % (time.time() - start_sort))
-
-        min_val = min(df['timestamp'])
-        max_val = max(df['timestamp'])
-        print("max_val - min_val =", max_val - min_val)
-        print("max_val - min_val =", (max_val - min_val) / adjust_divisor)
-        def adjust(x):
-            return (x - min_val) / adjust_divisor
-
-        # Sometimes, there's a bunch of data with WAY different timestamps -- like, several THOUSAND
-        # seconds different. So, I basically adjust all of that data so it fits within the interval
-        # of the rest of the data.
-        df['ts'] = df['timestamp'].map(adjust)
-        # df2 = df[((df['ts'] >= duration+5))]
-        # if len(df2) > 0:
-        #     min_val2 = min(df2['ts'])
-
-        #     def adjust2(x):
-        #         if x >= min_val2:
-        #             return x - min_val2
-        #         return x
-
-        #     df['ts'] = df['ts'].map(adjust2)
-
-        # df.to_csv("df.csv")
-        
-        print("Added `ts` column to DF in %f seconds" % (time.time() - st_time))
-    print(df)
-
-    print("Total number of points: %d" % len(df))
-    if plot_cost and 'cost' not in df.columns:
-        print("Computing cost column now...")
-        df['cost'] = df.apply(lambda row: compute_cost_of_operation(row), axis = 1)
-        df.to_csv("./nns.csv")
-    print("Done.")
-    cumulative_cost = [0]
-
-    st_time = time.time() 
-    # For each second of the workload, count all the data points that occur during that second.
-    # These are the points that we'll plot.
     if buckets_path is None:
+        # If we pass a single .txt file, then just create DataFrame from the .txt file.
+        # Otherwise, merge all .txt files in the specified directory.
+        if input_path.endswith(".txt") or input_path.endswith(".csv"):
+            print("Reading existing DF from file at '%s'" % input_path)
+            df = pd.read_csv(input_path, index_col=None, header=0)
+            print("Existing DF has the following columns: %s" % str(df.columns))
+            if len(df.columns) == 2:
+                df.columns = COLUMNS
+                print("Set DF's columns to %s" % str(COLUMNS))
+
+            print("Loaded existing DF in %f seconds" % (time.time() - start_time))
+        else:
+            print("input_path: " + input_path)
+            print("joined: " + str(os.path.join(input_path, "*.txt")))
+            all_files = glob.glob(os.path.join(input_path, "*.txt"))
+            li = []
+            print("Merging the following files: %s" % str(all_files))
+            # Merge the .txt files into a single DataFrame.
+            for filename in all_files:
+                print("Reading file: " + filename)
+                tmp_df = pd.read_csv(filename, index_col=None, header=0)
+                tmp_df.columns = COLUMNS
+                li.append(tmp_df)
+            df = pd.concat(li, axis=0, ignore_index=True)
+            df.columns = COLUMNS
+
+            print("Loaded data and created DF in %f seconds" % (time.time() - start_time))
+
+        st_time = time.time()
+        if 'ts' not in df.columns:
+            # Sort the DataFrame by timestamp.
+            print("Sorting DF and creating `ts` column now...")
+            start_sort = time.time()
+            df = df.sort_values('timestamp')
+            print("Sorted dataframe in %f seconds." % (time.time() - start_sort))
+
+            min_val = min(df['timestamp'])
+            max_val = max(df['timestamp'])
+            print("max_val - min_val =", max_val - min_val)
+            print("max_val - min_val =", (max_val - min_val) / adjust_divisor)
+            def adjust(x):
+                return (x - min_val) / adjust_divisor
+
+            # Sometimes, there's a bunch of data with WAY different timestamps -- like, several THOUSAND
+            # seconds different. So, I basically adjust all of that data so it fits within the interval
+            # of the rest of the data.
+            df['ts'] = df['timestamp'].map(adjust)
+            # df2 = df[((df['ts'] >= duration+5))]
+            # if len(df2) > 0:
+            #     min_val2 = min(df2['ts'])
+
+            #     def adjust2(x):
+            #         if x >= min_val2:
+            #             return x - min_val2
+            #         return x
+
+            #     df['ts'] = df['ts'].map(adjust2)
+
+            if save_dfs:
+                df.to_csv("df %s.csv" % label)
+
+            print("Added `ts` column to DF in %f seconds" % (time.time() - st_time))
+        print(df)
+        print("Total number of points: %d" % len(df))
+        if plot_cost and 'cost' not in df.columns:
+            print("Computing cost column now...")
+            df['cost'] = df.apply(lambda row: compute_cost_of_operation(row), axis = 1)
+            df.to_csv("./nns.csv")
+        print("Done.")
+        cumulative_cost = [0]
+
+        st_time = time.time()
+        # For each second of the workload, count all the data points that occur during that second.
+        # These are the points that we'll plot.
         print("Creating buckets now...")
         # For each second of the workload, count all the data points that occur during that second.
         # These are the points that we'll plot.
@@ -238,7 +240,7 @@ def plot(input:dict):
         print("Average Throughput: " + str(np.mean(buckets)) + " ops/sec.")
         print("Average Latency: " + str(df['ts'].mean()) + " ms.")
         print("Computed cost for all buckets in %f seconds" % (time.time() - st_time))
-        
+
         with open('%s_buckets.pkl' % label, 'wb') as bucket_file:
             pickle.dump(buckets, bucket_file)
             print("Wrote 'buckets' file for %s to file at ./%s_buckets.pkl" % (label, label))
@@ -276,9 +278,9 @@ def plot(input:dict):
 
         if not no_y_axis_labels and secondary_axis is not None:
             secondary_axis.set_ylabel("L-MDS NameNodes", color = 'black')
-    
-    axs.plot(list(range(len(buckets))), buckets, label = label, linestyle = linestyle, linewidth = linewidth, marker = marker, markevery=markevery, markersize = markersize, color = linecolor) 
-        
+
+    axs.plot(list(range(len(buckets))), buckets, label = label, linestyle = linestyle, linewidth = linewidth, marker = marker, markevery=markevery, markersize = markersize, color = linecolor)
+
     if plot_cost:
         cost_axs.plot(list(range(len(cumulative_cost))), cumulative_cost, linewidth = 4, color = '#E24A33', label = r'$\lambda$' + "MDS")
         hopsfs_cost = [0]
@@ -289,7 +291,7 @@ def plot(input:dict):
         cost_axs.plot(list(range(len(hopsfs_cost))), hopsfs_cost, linewidth = 4, color = '#348ABD', label = "HopsFS")
 
 with open(input_file_path, 'r') as input_file:
-    inputs = yaml.safe_load(input_file) 
+    inputs = yaml.safe_load(input_file)
 
 for i, input in enumerate(inputs):
     print("\n\n\nPlotting dataset #%d: '%s'. Path: '%s'" % (i, input["label"], input["path"]))
@@ -311,15 +313,15 @@ if args.legend:
     for ax in fig.axes:
         Line, Label = ax.get_legend_handles_labels()
         print("Label: '%s'" % str(Label))
-        
+
         if len(Label) == 0:
             continue
-        
+
         if Label[0] not in labels:
             lines.extend(Line)
             labels.extend(Label)
 
-    fig.legend(lines, labels, loc='upper left', prop={'size': 40}, bbox_to_anchor=(0.21, 0.975), framealpha=0.0, handlelength=1, labelspacing=0.2)
+    fig.legend(lines, labels, loc='upper left', prop={'size': 40}, bbox_to_anchor=(0.1225, 0.975), framealpha=0.0, handlelength=1, labelspacing=0.2)
 
 if plot_cost:
     cost_axs.set_xlabel("Time (seconds)", color = 'black')
